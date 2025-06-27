860d189ecb5c3da64b1b1eb83d78ad59e374d750
JPL <jplaurent33@gmail.com>
2025-06-27 13:46:20 +0200

    Add files via upload

---
+++ custom_components/loto_fdj/const.py
@@ -0,0 +1,7 @@
+from homeassistant.const import Platform
+
+DOMAIN = "loto_fdj"
+PLATFORMS: list[Platform] = [Platform.SENSOR]
+DEFAULT_NAME = "Loto FDJ"
+UPDATE_INTERVAL = 3600  # 1 heure
+FDJO_URL = "https://www.fdj.fr/jeux-de-tirage/loto/resultats"
