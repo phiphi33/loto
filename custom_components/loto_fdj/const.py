a535bc5f733d8c18bed1cc272f55d35e4b654848
JPL <jplaurent33@gmail.com>
2025-06-28 05:51:26 +0200

    const.py

--- custom_components/loto_fdj/const.py
+++ custom_components/loto_fdj/const.py
@@ -4,4 +4,5 @@ DOMAIN = "loto_fdj"
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 DEFAULT_NAME = "Loto FDJ"
 UPDATE_INTERVAL = 3600  # 1 heure
-FDJO_URL = "https://www.fdj.fr/jeux-de-tirage/loto/resultats"
+FDJO_URL = "https://www.fdj.fr/jeux-de-tirage/loto/resultats/mercredi-25-juin-2025"
+
